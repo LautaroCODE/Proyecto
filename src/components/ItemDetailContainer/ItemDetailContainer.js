aea40784ab7675c857f36c3fa1880db8a2c00935
Lautaro Vaz <lautaro.vaz@outlook.com>
2022-04-27 16:21:16 -0300

    Item Collection

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,6 @@
 import "./ItemDetailContainer.css";
 import { useState, useEffect } from "react";
-import { getProductsById } from "../../asyncmock";
+// import { getProductsById } from "../../asyncmock";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 import { firestoreDb } from "../../services/firebase";
@@ -25,11 +25,18 @@ const ItemDetailContainer = ({ setCart, cart }) => {
 		// 		setLoading(false);
 		// 	});
 
-		getDoc(doc(firestoreDb, "products", productId)).then((response) => {
-			console.log(response);
-			const product = { id: response.id, ...response.data() };
-			setProducts(product);
-		});
+		getDoc(doc(firestoreDb, "products", productId))
+			.then((response) => {
+				console.log(response);
+				const product = { id: response.id, ...response.data() };
+				setProducts(product);
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
 
 		return () => {
 			setProducts();
@@ -39,5 +46,3 @@ const ItemDetailContainer = ({ setCart, cart }) => {
 	return <div className="container-card">{loading ? <h1>Cargando...</h1> : products ? <ItemDetail {...products} setCart={setCart} cart={cart} /> : <h1>El producto no existe</h1>}</div>;
 };
 export default ItemDetailContainer;
-
-// CLASE 12 1:37:05
